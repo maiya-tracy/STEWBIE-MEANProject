d5838d5db789def5c9a16f77ebfa86d23624492a
Maiya Tracy <maiya.tracy@gmail.com>
2019-07-26 12:13:55 -0700

    minor edits

--- public/src/app/inspirationpage/inspirationpage.component.ts
+++ public/src/app/inspirationpage/inspirationpage.component.ts
@@ -8,25 +8,25 @@ import { HttpService } from '../http.service';
   styleUrls: ['./inspirationpage.component.css']
 })
 export class InspirationpageComponent implements OnInit {
-  search_bar: any;
-  test: any;
+  // search_bar: any;
+  // test: any;
   constructor(private _route: ActivatedRoute, private _router: Router, private _httpService: HttpService) { }
   ngOnInit() {
-    this.search_bar = "london";
-    this.getPlaces(this.search_bar);
-    this.getQuotes(this.search_bar);
+    // this.search_bar = "london";
+    // this.getPlaces(this.search_bar);
+    // this.getQuotes(this.search_bar);
   }
-  getPlaces(search_bar) {
-    this._httpService.getPlaces(search_bar).subscribe(data => {
-      console.log("**********", data);
-      this.test = data;
-      console.log(this.test)
-    })
-  }
-  getQuotes() {
-    this._httpService.getQuotes().subscribe(data => {
-      console.log("**********Quotes*************", data.Quotes);
-    })
-  }
+  // getPlaces(search_bar) {
+  //   this._httpService.getPlaces(search_bar).subscribe(data => {
+  //     console.log("**********", data);
+  //     this.test = data;
+  //     console.log(this.test)
+  //   })
+  // }
+  // getQuotes() {
+  //   this._httpService.getQuotes().subscribe(data => {
+  //     console.log("**********Quotes*************", data.Quotes);
+  //   })
+  // }
 
 }
